da5115d12103229b4c8571b9c008353ccdf6f20a
Michał Szczepański <szczepanskimichal99@gmail.com>
2022-06-30 07:34:03 +0200

    comment autologout in interceptor

--- src/app/core/interceptors/error.interceptor.ts
+++ src/app/core/interceptors/error.interceptor.ts
@@ -1,21 +1,23 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpResponse } from '@angular/common/http';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
 import { catchError, filter, Observable, tap, throwError } from 'rxjs';
 import { AuthService } from '../services/auth.service';
+import { MessageService } from 'primeng/api';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-  constructor(private authService: AuthService) {}
+  constructor(private messageService: MessageService, private authService: AuthService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    console.log(request);
     return next.handle(request).pipe(
-      catchError(err => {
+      catchError((err: HttpErrorResponse) => {
         console.log(err);
-        if (err.status === 401) {
-          this.authService.logout();
-          location.reload();
-        }
+        // if (err.status === 401 && !err.url?.includes('login')) {
+        //   this.authService.logout();
+        //   location.reload();
+        // }
+        if (err.status === 0 || String(err.status).startsWith('5'))
+          this.messageService.add({ severity: 'error', summary: 'Błąd', detail: 'Błąd serwera' });
         const error = err.error.message || err.statusText;
         return throwError(error);
       })
